098a23044dc5629d16a516d3e45c022114193631
tianhuizhou <tianhuizhou@users.noreply.github.com>
2021-03-13 15:06:54 -1000

    finished

--- app.js
+++ app.js
@@ -7,3 +7,44 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+const navLogo = document.querySelector('#navbar_logo');
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const serviceMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight')
+        serviceMenu.classList.remove('highlight')
+        return
+    }else if (window.innerWidth > 960 && scrollPos < 2345) {
+
+        aboutMenu.classList.remove('highlight');
+        serviceMenu.classList.add('highlight')
+        return
+    }
+
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem){
+        elem.classList.remove('highlight');
+    }
+}
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+menuLinks.addEventListener("click", hideMobileMenu)
+navLogo.addEventListener("click", hideMobileMenu)
